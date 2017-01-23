4cfe51e078da38a87f79fb5d4426b411c6fba381
SubhamSalaria <shubhamsalaria786.ss@gmail.com>
2017-01-23 18:24:06 -0500

    rechange

--- Assignment1/.Designer.cs
+++ Assignment1/.Designer.cs
@@ -231,7 +231,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 397);
+            this.ClientSize = new System.Drawing.Size(296, 397);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox4);
