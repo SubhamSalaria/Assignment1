a5d045cd444b4e0110ba75b9b35b90956762f55f
200335297 <Gowtham.Talluri@MyGeorgian.ca>
2017-01-27 23:53:15 -0500

    Implemented radio button functionality

--- Assignment1/.Designer.cs
+++ Assignment1/.Designer.cs
@@ -32,11 +32,11 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SalesBonusTextBox = new System.Windows.Forms.TextBox();
+            this.EmployeeIDTextBox = new System.Windows.Forms.TextBox();
+            this.EmployeesNameTextBox = new System.Windows.Forms.TextBox();
+            this.TotalSalesTextBox = new System.Windows.Forms.TextBox();
+            this.HoursWorkedTextBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -60,7 +60,6 @@
             this.label6.Size = new System.Drawing.Size(72, 13);
             this.label6.TabIndex = 4;
             this.label6.Text = "Sales Bonus :";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label5
             // 
@@ -71,7 +70,6 @@
             this.label5.Size = new System.Drawing.Size(66, 13);
             this.label5.TabIndex = 3;
             this.label5.Text = "Total Sales :";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label4
             // 
@@ -82,43 +80,42 @@
             this.label4.Size = new System.Drawing.Size(82, 13);
             this.label4.TabIndex = 2;
             this.label4.Text = "Hours Worked :";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // textBox5
+            // SalesBonusTextBox
             // 
-            this.textBox5.Location = new System.Drawing.Point(124, 327);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(143, 20);
-            this.textBox5.TabIndex = 4;
+            this.SalesBonusTextBox.Location = new System.Drawing.Point(124, 327);
+            this.SalesBonusTextBox.Name = "SalesBonusTextBox";
+            this.SalesBonusTextBox.ReadOnly = true;
+            this.SalesBonusTextBox.Size = new System.Drawing.Size(143, 20);
+            this.SalesBonusTextBox.TabIndex = 4;
             // 
-            // textBox4
+            // EmployeeIDTextBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(124, 215);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(143, 20);
-            this.textBox4.TabIndex = 3;
+            this.EmployeeIDTextBox.Location = new System.Drawing.Point(124, 215);
+            this.EmployeeIDTextBox.Name = "EmployeeIDTextBox";
+            this.EmployeeIDTextBox.Size = new System.Drawing.Size(143, 20);
+            this.EmployeeIDTextBox.TabIndex = 3;
             // 
-            // textBox3
+            // EmployeesNameTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(124, 176);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(143, 20);
-            this.textBox3.TabIndex = 2;
+            this.EmployeesNameTextBox.Location = new System.Drawing.Point(124, 176);
+            this.EmployeesNameTextBox.Name = "EmployeesNameTextBox";
+            this.EmployeesNameTextBox.Size = new System.Drawing.Size(143, 20);
+            this.EmployeesNameTextBox.TabIndex = 2;
             // 
-            // textBox2
+            // TotalSalesTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(124, 291);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(143, 20);
-            this.textBox2.TabIndex = 1;
+            this.TotalSalesTextBox.Location = new System.Drawing.Point(124, 291);
+            this.TotalSalesTextBox.Name = "TotalSalesTextBox";
+            this.TotalSalesTextBox.Size = new System.Drawing.Size(143, 20);
+            this.TotalSalesTextBox.TabIndex = 1;
             // 
-            // textBox1
+            // HoursWorkedTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(124, 254);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(143, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.HoursWorkedTextBox.Location = new System.Drawing.Point(124, 254);
+            this.HoursWorkedTextBox.Name = "HoursWorkedTextBox";
+            this.HoursWorkedTextBox.Size = new System.Drawing.Size(143, 20);
+            this.HoursWorkedTextBox.TabIndex = 0;
             // 
             // button1
             // 
@@ -126,9 +123,10 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
+            this.button1.Tag = "calculate";
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button_Click);
             // 
             // button2
             // 
@@ -136,9 +134,10 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 6;
+            this.button2.Tag = "print";
             this.button2.Text = "Print";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Button_Click);
             // 
             // button3
             // 
@@ -146,9 +145,10 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 7;
+            this.button3.Tag = "clear";
             this.button3.Text = "Clear";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.Button_Click);
             // 
             // pictureBox1
             // 
@@ -180,9 +180,10 @@
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(63, 17);
             this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
+            this.radioButton3.Tag = "spanish";
             this.radioButton3.Text = "Español";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -191,20 +192,24 @@
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(65, 17);
             this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
+            this.radioButton2.Tag = "french";
             this.radioButton2.Text = "Français";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
             this.radioButton1.Location = new System.Drawing.Point(38, 30);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(59, 17);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
+            this.radioButton1.Tag = "english";
             this.radioButton1.Text = "English";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // label3
             // 
@@ -215,7 +220,6 @@
             this.label3.Size = new System.Drawing.Size(71, 13);
             this.label3.TabIndex = 1;
             this.label3.Text = "Employee Id :";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
@@ -232,15 +236,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(293, 397);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.SalesBonusTextBox);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.EmployeeIDTextBox);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.EmployeesNameTextBox);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TotalSalesTextBox);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.HoursWorkedTextBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
@@ -250,7 +254,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SalesBouns";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -266,11 +270,11 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox SalesBonusTextBox;
+        private System.Windows.Forms.TextBox EmployeeIDTextBox;
+        private System.Windows.Forms.TextBox EmployeesNameTextBox;
+        private System.Windows.Forms.TextBox TotalSalesTextBox;
+        private System.Windows.Forms.TextBox HoursWorkedTextBox;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
---
+++ Assignment1/MailOrder.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+        private double _percentageOfHoursWorked;
+        private double _totalBonusAmount;
+        private double _salesBonus;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton option = sender as RadioButton;
+
+            switch (option.Tag.ToString())
+            {
+                case "english":
+                    this.label2.Text = "Employee\'s Name :";
+                    this.label3.Text = "Employee Id :";
+                    this.label4.Text = "Hours Worked :";
+                    this.label5.Text = "Total Sales :";
+                    this.label6.Text = "Sales Bonus :";
+                    this.button1.Text = "Calculate";
+                    this.button3.Text = "Clear";
+                    this.button2.Text = "Print";
+                    this.groupBox1.Text = "Languages";
+                    break;
+                case "french":
+                    this.label2.Text = "Le nom de l'employé :";
+                    this.label3.Text = "Employé ID :";
+                    this.label4.Text = "Heures travaillées :";
+                    this.label5.Text = "Ventes totales :";
+                    this.label6.Text = "Bonus de vente :";
+                    this.button1.Text = "Calculer";
+                    this.button3.Text = "Prochain";
+                    this.button2.Text = "Impression";
+                    this.groupBox1.Text = "Langues";
+                    break;
+                case "spanish":
+                    this.label2.Text = "Nombre del empleado :";
+                    this.label3.Text = "ID de empleado : ";
+                    this.label4.Text = "Horas trabajadas :";
+                    this.label5.Text = "Ventas totales :";
+                    this.label6.Text = "Bono de ventas: ";
+                    this.button1.Text = "Calcular";
+                    this.button3.Text = "Prochain";
+                    this.button2.Text = "Impresión";
+                    this.groupBox1.Text = "Idiomas";
+                    break;
+            }
+
+        }
+
+        //Calculates the Sales Bonus
+
+        private void CalculateButton_Click()
+        {
+
+            try
+            {
+                if (Convert.ToDouble(HoursWorkedTextBox.Text) > 0 && Convert.ToDouble(HoursWorkedTextBox.Text) <= 160)
+                {
+
+                    this._percentageOfHoursWorked = Convert.ToDouble(HoursWorkedTextBox.Text) / 160;
+                    if (TotalSalesTextBox.Text.Substring(0, 1) == "$")
+                    {
+                        this._totalBonusAmount = Convert.ToDouble(TotalSalesTextBox.Text.Substring(1)) * 0.02;
+                    }
+                    else
+                    {
+                        this._totalBonusAmount = Convert.ToDouble(TotalSalesTextBox.Text) * 0.02;
+                        TotalSalesTextBox.Text = "$" + TotalSalesTextBox.Text;
+                    }
+
+                    this._salesBonus = this._percentageOfHoursWorked * this._totalBonusAmount;
+                    SalesBonusTextBox.Text = this._salesBonus.ToString();
+
+                }
+                else
+                {
+                    MessageBox.Show("Hours Worked: Enter a valu from 0 to 160");
+                    clearAll();
+                }
+
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Invalid Data Entered", "Input Error");
+                // Debug.WriteLine(exception.Message);
+                clearAll();
+            }
+
+        }
+        //clears all fields
+        private void clearAll()
+        {
+            EmployeesNameTextBox.Focus();
+            EmployeesNameTextBox.Text = " ";
+            EmployeeIDTextBox.Text = " ";
+            HoursWorkedTextBox.Text = " ";
+            SalesBonusTextBox.Text = "";
+        }
+
+        // Prints the Messsage 
+        private void PrintButton_Click()
+        {
+            if ((SalesBonusTextBox.Text) != string.Empty)
+                MessageBox.Show("The form is being sent to the printer. Thank you!");
+
+            else
+                MessageBox.Show("Please fill the form");
+        }
+
+        //When ever buttons are clicked this function will be executed
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button click = sender as Button;
+
+            switch (click.Tag.ToString())
+            {
+                case "calculate":
+                    this.CalculateButton_Click();
+
+                    break;
+                case "print":
+                    this.PrintButton_Click();
+                    break;
+                case "clear":
+                    this.clearAll();
+                    break;
+
+
+            }
+        }
+    }
+}
